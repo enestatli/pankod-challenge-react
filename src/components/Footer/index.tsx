18cc483721b412e298c5ea2dd781aa90d4150d08
Hakan Han <eenestatli@gmail.com>
2021-02-26 23:05:01 +0300

    Patch: Footer & NavBar added

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,50 @@
+const Footer = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Terms and Conditions</li>
+        <li>Privacy and Policy</li>
+        <li>Collection Statement</li>
+        <li>Help</li>
+        <li>Manage Account</li>
+      </ul>
+      <p>Copyright ^C 2016 Demo Steraming. All Rights Reserved.</p>
+
+      <ul>
+        <li>
+          <img
+            src="../../assets/social/facebook-white.svg"
+            alt="facebook-logo"
+          />
+        </li>
+        <li>
+          <img src="../../assets/social/twitter-white.svg" alt="twitter-logo" />
+        </li>
+        <li>
+          <img
+            src="../../assets/social/instagram-white.svg"
+            alt="instagram-logo"
+          />
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <img src="../../assets/store/app-store.svg" alt="app-store-logo" />
+        </li>
+        <li>
+          <img src="../../assets/store/play-store.svg" alt="play-store-logo" />
+        </li>
+        <li>
+          <img
+            src="../../assets/store/windows-store.svg"
+            alt="windows-store-logo"
+          />
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+  return (
+    <div>
+      <h1>Demo Streaming</h1>
+      <ul>
+        <li>Login</li>
+        <li>Start your free trail</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
