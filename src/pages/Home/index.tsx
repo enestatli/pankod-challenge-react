5fa33d26d63ab6eb0ae2d4a95c81378d448143c6
Hakan Han <eenestatli@gmail.com>
2021-02-28 13:52:48 +0300

    Ignore: Export pages & CardList imported

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,13 @@
-import { useEffect } from 'react';
-import { fetchData } from '../../utils';
+import { CardList } from '../../components/CardList';
+
+import './styles.css';
 
 interface Props {}
 
 const Home = (props: Props) => {
   return (
     <div className="homepage">
-      <h2>Movies Card</h2>
-
-      <h2>Series Card</h2>
+      <CardList />
     </div>
   );
 };
---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+import Error from './Error';
+import Home from './Home';
+import Movies from './Movies';
+import Series from './Series';
+
+export { Error, Home, Movies, Series };
