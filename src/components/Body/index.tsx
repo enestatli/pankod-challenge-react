1fc19b03831049f7572a87015c971a99418f0e2d
Hakan Han <eenestatli@gmail.com>
2021-02-28 14:45:48 +0300

    New: Fixing Header&NavBar CSS / Added Body comp

---
+++ src/components/Body/index.tsx
@@ -0,0 +1,11 @@
+interface Props {}
+
+const Body = (props: Props) => {
+  return (
+    <div>
+      <p>I am Error maybe Loading</p>
+    </div>
+  );
+};
+
+export default Body;
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,16 +2,22 @@ import './styles.css';
 
 const NavBar = () => {
   return (
-    <div className="container">
+    <div className="navbar">
       <div className="wrap">
         <div className="left">
-          <h2>Demo Streaming</h2>
+          <h1>DEMO Streaming</h1>
         </div>
 
-        <ul className="right">
-          <li>Login</li>
-          <li>Start your free trail</li>
-        </ul>
+        <div className="right">
+          <ul className="right">
+            <li>
+              <button>Log in</button>
+            </li>
+            <li>
+              <button>Start your free trail</button>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
