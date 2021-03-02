78a7fe52de4b3841a9a69471cc033e0a33cc8284
Hakan Han <eenestatli@gmail.com>
2021-03-02 10:36:11 +0300

    Patch: Remove drop down arrow / Added defs

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,14 @@
 import { IMedia } from '../interfaces';
 import { apiService } from '../services';
 
-const callFetch = async (callback: (data: IMedia[]) => {}) => {
+//TODO: make it hook
+const callFetch = async (
+  callback: (data: IMedia[] | undefined | any) => {}
+) => {
   const res = await apiService.fetchData();
   if (!res || !res.entries || !res.total) {
     return;
   }
-
   callback(res.entries);
   return;
 };
